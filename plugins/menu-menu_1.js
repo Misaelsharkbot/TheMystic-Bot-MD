463eedb8736c04c2da565703d568b48300dcb60e
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-06-21 18:55:11 -0500

    Update menu-menu_1.js

--- plugins/menu-menu_1.js
+++ plugins/menu-menu_1.js
@@ -73,7 +73,7 @@ text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length
     
 let imagen1 = fs.readFileSync('./Menu2.jpg')
 let imagen2 = fs.readFileSync('./src/nuevobot.jpg') 
-let imagen3 = fs.readFileSync('./src/Pre Bot Publi.png')
+let imagen3 = fs.readFileSync('./src/porhub.png')
 let texto1 = `╭═─═─═─═─═─═─═╮
 ║     𝚃𝙷𝙴 𝙼𝚈𝚂𝚃𝙸𝙲 - 𝙱𝙾𝚃 - 𝙼𝙳
 ║╰═─═─═─═─═─══╯
